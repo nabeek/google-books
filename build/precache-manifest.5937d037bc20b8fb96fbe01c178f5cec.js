60b1dec8a3edd42d27a73da3fcc4de4947009f9a
nabeek <nabeek@gmail.com>
2020-07-29 19:45:36 -0600

    Moved build file outside of client

---
+++ build/precache-manifest.5937d037bc20b8fb96fbe01c178f5cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4827699f99a99cba65ad",
+    "url": "/static/css/main.057f8a5a.chunk.css"
+  },
+  {
+    "revision": "4827699f99a99cba65ad",
+    "url": "/static/js/main.ad1b270d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10e78932a208a9a64a3b",
+    "url": "/static/js/2.e707aa5a.chunk.js"
+  },
+  {
+    "revision": "87920c095bb073fc69922946a0d14781",
+    "url": "/index.html"
+  }
+];
